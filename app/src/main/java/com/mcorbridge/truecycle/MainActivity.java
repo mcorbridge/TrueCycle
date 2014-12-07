68648fa91b3ec37cbd727e94f443ce9b213aac6b
michael corbridge <mcorbridge@gmail.com>
2014-12-07 15:19:03 -0500

    adding interation

--- app/src/main/java/com/mcorbridge/truecycle/MainActivity.java
+++ app/src/main/java/com/mcorbridge/truecycle/MainActivity.java
@@ -2,20 +2,41 @@ package com.mcorbridge.truecycle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.mcorbridge.truecycle.data.men.MensWattData;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends Activity {
+
+
+    private ArrayList proWattData;
+    private ArrayList domesticProWattData;
+    private ArrayList cat1WattData;
+    private ArrayList cat2WattData;
+    private ArrayList cat3WattData;
+    private ArrayList cat4WattData;
+    private ArrayList cat5WattData;
+    private ArrayList recWattData;
+
+    private String currentCategory;
+    private String currentLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        getMensWattData();
     }
 
 
@@ -41,18 +62,73 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void getMensWattData(){
-        MensWattData mensWattData = new MensWattData();
-        mensWattData.setCyclistWeight(80.0);
-        mensWattData.getProWattData();
-        mensWattData.getDomesticProWattData();
-        mensWattData.getCat1WattData();
-        mensWattData.getCat2WattData();
-        mensWattData.getCat3WattData();
-        mensWattData.getCat4WattData();
-        mensWattData.getCat5WattData();
-        mensWattData.getRecWattData();
+    public void doViewInputWeight(View v){
+        setContentView(R.layout.activity_input_weight);
     }
 
+    public void doSubmit(View v){
+        EditText mEdit = (EditText)findViewById(R.id.editText);
+        Integer intWeight = Integer.parseInt(mEdit.getText().toString());
+        Double dblWeight = (double)intWeight;
+
+        if(findViewById(R.id.radioKilogram).isSelected()){
+            getMensWattData(dblWeight);
+        }else{
+            getMensWattData(dblWeight * 0.453592);
+        }
+    }
+
+    public void getMensWattData(Double cyclistWeight){
+        MensWattData mensWattData = new MensWattData();
+        mensWattData.setCyclistWeight(cyclistWeight);
+        proWattData = mensWattData.getProWattData();
+        domesticProWattData = mensWattData.getDomesticProWattData();
+        cat1WattData = mensWattData.getCat1WattData();
+        cat2WattData = mensWattData.getCat2WattData();
+        cat3WattData = mensWattData.getCat3WattData();
+        cat4WattData = mensWattData.getCat4WattData();
+        cat5WattData = mensWattData.getCat5WattData();
+        recWattData =  mensWattData.getRecWattData();
+
+        setContentView(R.layout.activity_select_level);
+    }
+
+    public void doSubmitCategory(View v){
+
+        RadioGroup g = (RadioGroup) findViewById(R.id.radioGroupCategory);
+        int selected = g.getCheckedRadioButtonId();
+        RadioButton b = (RadioButton) findViewById(selected);
+        currentCategory = b.getText().toString();
+
+        setContentView(R.layout.activity_show_effort);
+        TextView mTextView = (TextView) findViewById(R.id.textView2);
+        mTextView.setText(currentCategory + " Watts");
+    }
+
+    public void doSubmitEffort(View v){
+        setContentView(R.layout.activity_show_watts);
+        TextView mTextView = (TextView) findViewById(R.id.textView3);
+        mTextView.setText(currentCategory + " Watts");
+
+        // 5 wec max
+        ArrayList list5secMax = (ArrayList)proWattData.get(0);
+
+        ArrayList<String> modifiedList = parseDouble(list5secMax);
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, modifiedList);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(itemsAdapter);
+    }
+
+    private ArrayList parseDouble(ArrayList arrayList){
+        ArrayList<String> arString = new ArrayList<String>();
+        for (int i = 0; i < arrayList.size(); i++) {
+            String s = arrayList.get(i).toString();
+            String[] parts = s.split("\\.");
+            arString.add(i,parts[0]);
+        }
+        return arString;
+    }
 
 }
